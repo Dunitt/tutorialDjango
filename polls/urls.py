52ee7622bfd84e1688b9cc7e64fbaf3d29dce25d
Dunitt Monagas <dunitt.monagas@example.com>
2017-10-28 08:11:07 -0400

    Parte 3.

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,19 @@ from django.conf.urls import url
 
 from . import views
 
+app_name= "polls"
 urlpatterns = [
+
+	# ex: /polls/
     url(r'^$', views.index, name='index'),
+
+     # ex: /polls/<question_id>/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name="detail"),
+
+    # ex: /polls/<question_id>/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name="results"),
+
+    # ex: /polls/<question_id>/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name="vote"),
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,31 @@
 
+from django.http import Http404
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 
+from .models import Question
+
+# ex: /polls/
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+	lastest_question_list= Question.objects.order_by("-pub_date")[:5]
+	contex= {"lastest_question_list": lastest_question_list}
+
+	return render(request, "polls/index.html", contex)
+
+# ex: /polls/<question_id>/
+def detail(request, question_id):
+
+	question= get_object_or_404(Question, pk= question_id)
+
+	return render(request, "polls/detail.html", {"question": question})
+
+# ex: /polls/<question_id>/results/
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+# ex: /polls/<question_id>/vote/
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
